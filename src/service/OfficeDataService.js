b7a38d12cfeedf9db2bff393bd604c82d084a879
Madhan <820122@cognizant.com>
2019-12-18 11:01:37 -0500

    Service methods for getting offices

--- src/service/OfficeDataService.js
+++ src/service/OfficeDataService.js
@@ -1,17 +1,33 @@
 import axios from 'axios';
 
 
-const OFFICE_API_URL = 'https://sunshine-office-crud.cfapps.io/'
+const OFFICE_API_URL = 'https://sunshine-fe-ms.cfapps.io'
+const AuthStr = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzY3MjY2NDQsInVzZXJfbmFtZSI6ImFkbWluIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiJmMjA3M2MzNS02YjUyLTRlZTUtODY3Ni03ZmZkODdhODg2OTkiLCJjbGllbnRfaWQiOiJDbGllbnRJZCIsInNjb3BlIjpbImFsbCJdfQ.qOPui8E4dB3wnZGenhoPJ5ITNF6WssQfA2ffbkLyQHk'
 
 class OfficeDataService {
 
-    updateOffice() {
-        return axios.put(`${OFFICE_API_URL}/offices/office/${id}`)
+    getAllOffices(){
+        
+        return axios.get(OFFICE_API_URL + '/api/offices', {headers: { Authorization: AuthStr }})
+            .then(response => {
+                console.log(response.data);
+                return response.data;
+            })
     }
 
-    createOffice() {
-        return axios.post(`${OFFICE_API_URL}/offices`)
+    createOffice(office) {
+        console.log("creating office");
+        return axios.post(`${OFFICE_API_URL}/api/offices`, office, {headers: { Authorization: AuthStr }})
+                    .then(res => {
+                        console.log(res.data)
+                    })
     }
+
+    updateOffice() {
+        // return axios.put(`${OFFICE_API_URL}/offices/office/${id}`)
+    }
+
+   
 }
 
 export default new OfficeDataService()
