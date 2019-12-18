493aeda58dafa1d7359b8e62122d7734133e0cfa
Ahmed ElMallah <ahmed.elmalah@live.com>
2019-12-18 12:25:03 -0500

    Chitra fixed the issue

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,8 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
   // Import Main styles for this application
   @import '@/styles/bootstrap.scss';
   @import '@/styles/app.scss';
