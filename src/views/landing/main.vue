50a3c6f3d3128fde7ad9d1aa6916882ad63d87ee
William King <wpk12345@gmail.com>
2019-12-23 08:57:16 -0500

    small update to map

--- src/views/landing/main.vue
+++ src/views/landing/main.vue
@@ -2,13 +2,16 @@
   <div class="div-container">
     <h1 id="title">Cognizant Location Resource Manager</h1>
     <div id="appLanding">
-      <!-- <img
-        src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDn5REd-ifbdS-DtVviYTCaIuGEKVhMdeA&center=29.968766460941684,-75.28427080494306&zoom=2&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative%7Celement:geometry%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:administrative.neighborhood%7Cvisibility:off&style=feature:poi%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Cvisibility:off&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit%7Cvisibility:off&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=480x360"
-        alt
-      />-->
       <div>
+        <!-- <div class="div continentList">
+          <ul>
+            <li @click="centerMap">North America</li>
+            <li>Europe</li>
+            <li>Asia</li>
+          </ul>
+        </div> -->
         <div>
-          <h2>Search and add a pin</h2>
+          <!-- <h2>Search and add a pin</h2> -->
           <label>
             <gmap-autocomplete @place_changed="setPlace"></gmap-autocomplete>
             <button @click="addMarker">Add</button>
@@ -17,9 +20,9 @@
         </div>
         <br />
         <gmap-map
-          :center="center"
+          :center="center[0]"
           :options="{styles: styles}"
-          style="width:100%;  height: 400px;"
+          style="width:100%;  height: 450px;"
           :zoom="2"
         >
           <gmap-marker
@@ -42,7 +45,12 @@ export default {
   data() {
     return {
       loading: false,
-      center: { lat: 45.508, lng: -73.587 },
+      center: [
+        { lat: 30.508, lng: -73.587 },
+        { lat: 35.001237, lng: -88.15932 },
+        { lat: 20.698587, lng: 78.26626 },
+        { lat: 49.883319, lng: 16.418917 }
+      ],
       markers: [],
       places: [],
       currentPlace: null,
@@ -286,6 +294,10 @@ export default {
     // receives a place object via the autocomplete component
     setPlace(place) {
       this.currentPlace = place;
+    },
+    centerMap() {
+      alert('map centered');
+      this.center = this.center[3];
     },
     addMarker() {
       if (this.currentPlace) {
